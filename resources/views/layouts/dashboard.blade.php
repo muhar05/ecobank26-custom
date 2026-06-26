110b1e84aaffa5e8bcfd5bff60495c6459c4177c
Muhar <ferdiansyahmuh5@gmail.com>
2026-06-26 23:57:59 +0700

    feat: update favicon links in layout files for consistent branding

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Ecobank026') }}</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico?v=2') }}">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700&display=swap" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
